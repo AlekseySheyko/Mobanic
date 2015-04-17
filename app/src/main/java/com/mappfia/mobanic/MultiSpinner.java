6fb2ec998d114918bb6ef2cca61c23386e2e425b
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-04-18 00:03:18 +0300

    Set hind, model name or models count to spinner

--- app/src/main/java/com/mappfia/mobanic/MultiSpinner.java
+++ app/src/main/java/com/mappfia/mobanic/MultiSpinner.java
@@ -11,15 +11,13 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MultiSpinner extends Spinner implements
-        OnMultiChoiceClickListener {
+public class MultiSpinner extends Spinner
+        implements OnMultiChoiceClickListener {
 
-    private boolean[] selected;
-    private String defaultText;
     private MultiSpinnerListener mListener;
-
     private ArrayAdapter<String> mAdapter;
     private List<String> mMakeStrings;
+    private boolean[] mCheckboxes;
 
     public MultiSpinner(Context context, AttributeSet attrSet) {
         super(context, attrSet);
@@ -33,51 +31,51 @@ public class MultiSpinner extends Spinner implements
     }
 
     @Override
-    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-        if (isChecked)
-            selected[which] = true;
-        else
-            selected[which] = false;
+    public void onClick(DialogInterface dialog, int position, boolean isChecked) {
+        mCheckboxes[position] = isChecked;
     }
 
     public void updateSelectedItems() {
-        // refresh text on spinner
-        StringBuffer spinnerBuffer = new StringBuffer();
-        boolean someUnselected = false;
-        for (int i = 0; i < mMakeStrings.size(); i++) {
-            if (selected[i] == true) {
-                spinnerBuffer.append(mMakeStrings.get(i));
-                spinnerBuffer.append(", ");
-            } else {
-                someUnselected = true;
+        boolean nothingSelected = true;
+        String selectedMake = null;
+
+        int selectedItemsCount = 0;
+        for (int i = 0; i < mCheckboxes.length; i++) {
+            if (mCheckboxes[i]) {
+                selectedItemsCount++;
+                selectedMake = mAdapter.getItem(i + 1);
             }
         }
+
         String spinnerText;
-        if (someUnselected) {
-            spinnerText = spinnerBuffer.toString();
-            if (spinnerText.length() > 2)
-                spinnerText = spinnerText.substring(0, spinnerText.length() - 2);
+        if (selectedItemsCount == 0) {
+            spinnerText = "Make";
+        } else if (selectedItemsCount == 1) {
+            spinnerText = selectedMake;
         } else {
-            spinnerText = defaultText;
+            spinnerText = selectedItemsCount + " models";
         }
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
                 android.R.layout.simple_spinner_item,
-                new String[] { spinnerText });
+                new String[]{spinnerText});
         setAdapter(adapter);
-        mListener.onItemsSelected(selected);
+        mListener.onItemsSelected(mCheckboxes);
     }
 
     @Override
     public boolean performClick() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setMultiChoiceItems(
-                mMakeStrings.toArray(new CharSequence[mMakeStrings.size()]), selected, this);
+                mMakeStrings.toArray(new CharSequence[mMakeStrings.size()]),
+                mCheckboxes,
+                this);
         builder.setPositiveButton("Set", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        updateSelectedItems();
-                    }
-                });
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                updateSelectedItems();
+            }
+        });
         builder.show();
         return true;
     }
@@ -88,13 +86,10 @@ public class MultiSpinner extends Spinner implements
 
         mListener = listener;
 
-        // all selected by default
-        selected = new boolean[makeStrings.size()];
-        for (int i = 0; i < selected.length; i++)
-            selected[i] = true;
+        mCheckboxes = new boolean[makeStrings.size()];
     }
 
     public interface MultiSpinnerListener {
-        public void onItemsSelected(boolean[] selected);
+        void onItemsSelected(boolean[] selected);
     }
 }
