578193dac5583e39dbe15268078126273855da5d
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-04-23 20:10:51 +0300

    Save state of "contact us" fields

--- app/src/main/java/com/mappfia/mobanic/activities/ContactActivity.java
+++ app/src/main/java/com/mappfia/mobanic/activities/ContactActivity.java
@@ -1,9 +1,12 @@
 package com.mappfia.mobanic.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.mappfia.mobanic.R;
@@ -11,11 +14,24 @@ import com.mappfia.mobanic.R;
 
 public class ContactActivity extends ActionBarActivity {
 
+    private EditText mNameEditText;
+    private EditText mEmailEditText;
+    private EditText mPhoneEditText;
+    private EditText mMessageEditText;
+
+    private SharedPreferences mSharedPrefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
-        // TODO: Store fields input in shared prefs and restore on activity resume
+
+        mNameEditText = (EditText) findViewById(R.id.name);
+        mEmailEditText = (EditText) findViewById(R.id.email);
+        mPhoneEditText = (EditText) findViewById(R.id.phone);
+        mMessageEditText = (EditText) findViewById(R.id.message);
+
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     @Override
@@ -32,5 +48,41 @@ public class ContactActivity extends ActionBarActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSharedPrefs.edit()
+                .putString("name", mNameEditText.getText().toString())
+                .putString("email", mEmailEditText.getText().toString())
+                .putString("phone", mPhoneEditText.getText().toString())
+                .apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name = mSharedPrefs.getString("name", "");
+        String email = mSharedPrefs.getString("email", "");
+        String phone = mSharedPrefs.getString("phone", "");
+        String message = mSharedPrefs.getString("message", "");
+
+        mNameEditText.setText(name);
+        mEmailEditText.setText(email);
+        mPhoneEditText.setText(phone);
+
+        if (message.isEmpty()) {
+            mMessageEditText.requestFocus();
+        }
+        if (phone.isEmpty()) {
+            mPhoneEditText.requestFocus();
+        }
+        if (email.isEmpty()) {
+            mEmailEditText.requestFocus();
+        }
+        if (name.isEmpty()) {
+            mNameEditText.requestFocus();
+        }
     }
 }
