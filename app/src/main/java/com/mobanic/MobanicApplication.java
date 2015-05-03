01c9faa4e298f6d50dd4c7691cba72963ebe8a17
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-05-03 14:55:26 +0300

    Enable crash reporting to Parse

--- app/src/main/java/com/mobanic/MobanicApplication.java
+++ app/src/main/java/com/mobanic/MobanicApplication.java
@@ -3,6 +3,7 @@ package com.mobanic;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseCrashReporting;
 import com.parse.ParseInstallation;
 import com.parse.ParsePush;
 
@@ -13,6 +14,7 @@ public class MobanicApplication extends Application {
         super.onCreate();
 
         Parse.enableLocalDatastore(this);
+        ParseCrashReporting.enable(this);
         Parse.initialize(this, "asQfJUeEXdJnbfvG2iefkvjhVpcmIq2GnOp5x5P2", "rsHyNBGIAmo7eFIUfmjvAm5kOMrwlMCDz7d5El6Z");
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
